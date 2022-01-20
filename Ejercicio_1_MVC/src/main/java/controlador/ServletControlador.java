fd38a1f64980c644c5a46d3705655b7f41c235ef
Marcosss2 <marcosparra20@hotmail.com>
2022-01-20 08:42:09 +0100

    .
    .

---
+++ Ejercicio_1_MVC/src/main/java/controlador/ServletControlador.java
@@ -0,0 +1,22 @@
+
+package controlador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       
+        //1. PROCESAMOS LOS PARÁMETROS
+        String base = request.getParameter("base");
+    }
+    
+    
+}
